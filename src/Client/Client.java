f70d66d89e4228876c7d058db6396527f6ba98e3
Williamxu98 <xuwilliam01@gmail.com>
2016-09-17 15:01:07 -0400

    Add back

---
+++ src/Client/Client.java
@@ -0,0 +1,87 @@
+package Client;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
+public class Client implements Runnable {
+
+	public static final String IP = "127.0.0.1";
+	public static final int PORT = 4200;
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+
+		System.out.print("Enter your name: ");
+		String name = scan.nextLine();
+		new Client(name, IP, PORT);
+
+		scan.close();
+	}
+
+	Socket socket;
+	BufferedReader input;
+	PrintWriter output;
+	String name;
+
+	public Client(String name, String IP, int port) {
+		try {
+
+			socket = new Socket(IP, port);
+
+			System.out.println("Connected to server");
+
+			this.name = name;
+			input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			output = new PrintWriter(socket.getOutputStream());
+
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		Thread thread = new Thread(new Writer());
+		thread.start();
+
+		run();
+
+	}
+
+	@Override
+	public void run() {
+		while (true) {
+			try {
+				String message = input.readLine();
+
+				System.out.println(message);
+
+			} catch (IOException e) {
+
+				break;
+			}
+
+		}
+	}
+
+	class Writer implements Runnable {
+
+		@Override
+		public void run() {
+			while (true) {
+				Scanner scan = new Scanner(System.in);
+
+				String command = scan.nextLine();
+				output.println(command);
+				output.flush();
+			}
+
+		}
+
+	}
+
+}
---
+++ src/Objects/Signpost.java
@@ -0,0 +1,15 @@
+package Objects;
+
+public class Signpost extends Object{
+
+	String title;
+	String url;
+	
+	public Signpost(int x, int y, String title, String url) {
+		super(x, y, Object.SIGNPOST_IMAGE);
+		
+	}
+	
+	
+
+}
