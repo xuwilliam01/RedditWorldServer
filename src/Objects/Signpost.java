896e1c5081a1162ce2111406f1be16a80f5d5948
William Xu <William Xu@10.21.100.158>
2016-09-17 14:38:53 -0400

    Started work on signposts and world

--- src/Objects/Signpost.java
+++ src/Objects/Signpost.java
@@ -5,8 +5,15 @@ public class Signpost extends Object{
 	String title;
 	String url;
 	
+	int width;
+	int height;
+	
 	public Signpost(int x, int y, String title, String url) {
 		super(x, y, Object.SIGNPOST_IMAGE);
+		
+		width = 16*4;
+		height = 16*4;
+				
 		
 	}
 	
--- src/Server/World.java
+++ src/Server/World.java
@@ -4,8 +4,12 @@ import java.util.ArrayList;
 
 public class World {
 
+	
+	
 	public final static int TILE_SIZE = 16*4;
 	ArrayList<Object>[][] objectGrid;
+	
+	ArrayList<Object> objects= new ArrayList<Object>();
 	
 	public World()
 	{
