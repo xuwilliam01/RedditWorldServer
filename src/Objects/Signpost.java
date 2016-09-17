74511b306679fec708b325961658ab73565e9f48
changangela <angelachang27@gmail.com>
2016-09-17 16:36:38 -0400

    Added properties to Post Class

--- src/Objects/Signpost.java
+++ src/Objects/Signpost.java
@@ -4,7 +4,28 @@ public class Signpost extends Object{
 
 	String title;
 	String url;
-	
+
+	String subreddit;
+	String author;
+	int points;
+	int numComments;
+	String permalink;
+	String domain;
+	String id;
+
+	String getDetails(){
+		String details=author +" posted this and got " +numComments +" replies";
+		return details;
+	}
+
+	String getTitle(){
+		return title;
+	}
+
+	String getScore(){
+		return Integer.toString(points);
+	}
+
 	public Signpost(int x, int y, String title, String url) {
 		super(x, y, Object.SIGNPOST_IMAGE);
 		
