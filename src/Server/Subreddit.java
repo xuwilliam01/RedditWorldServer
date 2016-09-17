457ee290b3a25f7066471e2512a6d5266914e293
Williamxu98 <xuwilliam01@gmail.com>
2016-09-17 15:05:00 -0400

    Add back

---
+++ src/Server/Subreddit.java
@@ -0,0 +1,21 @@
+package Server;
+
+import java.util.ArrayList;
+
+import Objects.Signpost;
+
+public class Subreddit {
+
+	
+	public final static int SIDE_LENGTH = 1000;
+	public final static int TILE_SIZE = 16;
+	ArrayList<Signpost>[][] signGrid;
+	
+	ArrayList<Signpost> signs= new ArrayList<Signpost>();
+	
+	public Subreddit()
+	{
+		
+	}
+	
+}
