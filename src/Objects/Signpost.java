4adea40bdbc011c3e86c9b49691cc752d484356c
Williamxu98 <xuwilliam01@gmail.com>
2016-09-17 14:59:21 -0400

    Updated signpost

--- src/Objects/Signpost.java
+++ src/Objects/Signpost.java
@@ -5,15 +5,8 @@ public class Signpost extends Object{
 	String title;
 	String url;
 	
-	int width;
-	int height;
-	
 	public Signpost(int x, int y, String title, String url) {
 		super(x, y, Object.SIGNPOST_IMAGE);
-		
-		width = 16*4;
-		height = 16*4;
-				
 		
 	}
 	
